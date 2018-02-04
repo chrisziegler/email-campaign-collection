ce8524868f56152d40e2bf20af9cf94c76df6e40
chrisziegler <chrisrziegler@gmail.com>
2018-02-03 23:39:35 -0800

    cleaned up comments, added .vscode to gitignore

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,12 +10,8 @@ module.exports = app => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
-  // we learned that passport automatically attaches user to req
-  // passport also automatically attaches something called logout()
-  // logout takes the cookie and kills it
   app.get('/api/logout', (req, res) => {
     req.logout();
-    // for now, send the user something back that acknowledges logout
     res.send(req.user);
   });
 
