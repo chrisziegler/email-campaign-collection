cec0b25ab5c5075c989bad2c0acdb1c7eab13f19
chrisziegler <chrisrziegler@gmail.com>
2018-02-05 23:30:30 -0800

    async refactor added react frontend client to directory structure

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,16 +23,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      // we already have a record with the given profile ID
+      if (existingUser) {
+        // since this now gets returned, don't need else block
+        return done(null, existingUser);
+      }
+      // we don't have a user record with this ID, make a new record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
