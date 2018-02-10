1bb15dff25b0acefd06b7c3fc5cf691f216cbf19
chrisziegler <chrisrziegler@gmail.com>
2018-02-09 20:53:35 -0800

    Added Auth aware routes for header navigation, began Stripe payment integration

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    // since we're slecting between 3 values - use switch statement
+    // testing with dummy values
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
-        <div className="nav-wrapper">
-          <a className="left brand-logo">CampaignMail</a>
-          <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
-          </ul>
+        <div className="nav-wrapper light-green darken-1">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            CampaignMail
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,11 +8,20 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    // res.send(req.user);
+    // rather than send the user back there User model
+    // which is always going to be empty anyways
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
