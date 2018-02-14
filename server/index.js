79d45408ab7ac4350b28b6ae8669fda5157ffe4e
chrisziegler <chrisrziegler@gmail.com>
2018-02-13 21:57:55 -0800

    Added billing and client side app with deployment configuration

--- server/index.js
+++ server/index.js
@@ -29,6 +29,19 @@ app.use(passport.session());
 // which we immediately call with the app object
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+// Express production environment config
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like the main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
