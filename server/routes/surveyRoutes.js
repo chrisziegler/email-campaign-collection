9bd0de578fdb1856b2253294b75860d2f090890e
chrisziegler <chrisrziegler@gmail.com>
2018-02-28 18:34:32 -0800

    added webhook filtering to surveyRoute returning an object (events) to query MongoDb by

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -19,19 +19,24 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, event => {
-      const pathname = new URL(event.url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      const match = p.test(pathname);
-      if (match) {
-        return {
-          email: event.email,
-          surveyId: match.surveyId,
-          choice: match.choice
-        };
-      }
-    });
+    // no need to redefine this regex-like pattern each iteration
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
     console.log(events);
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
